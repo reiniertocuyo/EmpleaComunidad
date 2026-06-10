167fe914246b28cbfc32ec004e61d4320ff797d2
Reinier Tocuyo <reiniertocuto@gmail.com>
2026-06-10 10:12:41 -0400

    coneccion ssl

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def procesar_subida_cv(archivo_formulario, usuario_id):
 
 # === CONFIGURACIÓN SEGURA DE CORREO ===
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
